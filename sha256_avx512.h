800f657bb425bd7574f01385aaa83c8244580f6d
Aleksander <poczta@zielar.pl>
2025-06-06 00:36:00 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -1,8 +1,9 @@
 #ifndef SHA256_AVX512_H
 #define SHA256_AVX512_H
 
+#include <immintrin.h>
 #include <stdint.h>
 
-void sha256_avx512_16B(const uint8_t* data[16], unsigned char* hash[16]);
+void sha256avx512_16B(const uint8_t* inputs[16], uint8_t* outputs[16]);
 
-#endif  // SHA256_AVX512_H
+#endif // SHA256_AVX512_H
