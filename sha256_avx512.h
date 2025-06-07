0e15299718f386e18f6f8dd9bf02c9bdd69ba203
Aleksander <poczta@zielar.pl>
2025-06-07 05:05:38 +0200

    Update sha256_avx512.h

--- sha256_avx512.h
+++ sha256_avx512.h
@@ -4,6 +4,8 @@
 #include <immintrin.h>
 #include <stdint.h>
 
+// Processes 16 blocks of 64 bytes each (one SHA-256 block per input).
+// Each outputs[i] receives 32-byte hash for inputs[i].
 void sha256avx512_16B(const uint8_t* inputs[16], uint8_t* outputs[16]);
 
-#endif // SHA256_AVX512_H
+#endif  // SHA256_AVX512_H
