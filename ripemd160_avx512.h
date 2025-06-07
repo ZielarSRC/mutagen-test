ffba9a7573c16ca14835ff9a80b5fb181961a9ce
Aleksander <poczta@zielar.pl>
2025-06-07 05:06:16 +0200

    Update ripemd160_avx512.h

--- ripemd160_avx512.h
+++ ripemd160_avx512.h
@@ -5,8 +5,11 @@
 #include <stdint.h>
 
 namespace ripemd160avx512 {
-// Process 16 blocks of 32 bytes each
+
+// Processes 16 blocks of 32 bytes each.
+// Each outputs[i] receives a 20-byte RIPEMD-160 hash for inputs[i].
 void ripemd160avx512_16(const uint8_t* inputs[16], uint8_t* outputs[16]);
+
 }  // namespace ripemd160avx512
 
 #endif  // RIPEMD160_AVX512_H
